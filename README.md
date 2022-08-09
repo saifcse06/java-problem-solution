4a983a46ad43bb62ba2661b5da98ead08728cce7
Saif Uddin <saif.uddin@sslwireless.com>
2022-08-09 22:36:11 +0600

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # java-problem-solution
-Java Problem And Solution
+Java Problem And Solution Simple
+
+## List Question:
+
+    1) Write a program in java to check whether a number is prime or not?
+    2) Write a program to find a string is palindrome or not? 
+    3) Write a program to find the factorial of a number in java? 
+    4) Java Code to find whether a number Armstrong or not? 
+    5) Java Program to generate Fibonacci series?
+    6) Write a program in java to reverse a number?  
---
+++ Solutions.java
@@ -0,0 +1,98 @@
+public class Solutions {
+    static int number1=0,number2=1,number3=0;    
+    /*
+     * Q:01
+     */
+    public static int checkPrimeNumber(int n) {
+        if (n == 1) {
+            return 0;
+        }
+        for (int index = 2; index <= n / 2; index++) {
+            if (n % index == 0) {
+                return 0;
+            }
+        }
+        return 1;
+    }
+
+    /*
+     * Q:02
+     */
+    public static int Palindrome(int n) {
+        int temp = n;
+        int newnumber = 0;
+        while (temp >= 1) {
+            int remainNumber = temp % 10;
+            newnumber = newnumber * 10 + remainNumber;
+            temp = temp / 10;
+        }
+        if (newnumber == n) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+    /*
+     * Q:03
+     */
+
+    public static int Factorial(int n) {
+        if (n <= 1)
+            return 1;
+        else
+            return n * Factorial(n - 1);
+    }
+
+    /*
+     * Q:04
+     */
+    public static int armstrongCheck(int n) {
+        int temp = n, isArmst = 0, sum = 0, count = 0;
+
+        while (n != 0) {
+            n = n / 10;
+            count = count + 1;
+        }
+        while (temp > n) {
+            int remain = temp % 10;
+            sum = (int) (sum + Math.pow(remain, count));
+            temp = temp / 10;
+        }
+        if (n == temp)
+            isArmst = 1;
+        return isArmst;
+    }
+    /*
+     * Q:05
+     */
+    static void Fibonacci(int n) { 
+        if(n>0){     
+            number3 = number1+number2;
+            number1=number2;
+            number2=number3; 
+            System.out.print(" "+number3);   
+            Fibonacci(n-1);
+        }
+    }
+    /*
+     * Q:06
+     */
+
+     public static int reverseNumber(int n) {
+        int revNumber=0,remain=0;
+        while (n>=1) {
+            remain = n%10;
+            revNumber = revNumber*10+remain;
+            n = n/10;
+        }
+        return revNumber;
+     }
+    public static void main(String[] args) {
+        // System.out.println(checkPrimeNumber(4));
+        // System.out.println(Palindrome(131));
+        // System.out.println(Factorial(5));
+        //System.out.println(armstrongCheck(135));   
+        //Fibonacci(10);
+        //System.out.println(reverseNumber(123));
+    }
+}      
